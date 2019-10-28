753a65062a04b99afcde6e836fd93ee01a0f3eca
danorlarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-10-28 14:50:53 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "mydb"
+});
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO customersv2 (name, address) VALUES ('Mathias Inc', 'Highway 69')"; //SQL is the query that will be executed on the server
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+  });
