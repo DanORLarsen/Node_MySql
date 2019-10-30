d3c59b695f8efb98931bec64e0ca319bcf8b03da
danorlarsen <42993978+DanORLarsen@users.noreply.github.com>
2019-10-30 13:11:19 +0100

    Better display of data

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,13 @@ router.get('/', function(req, res, next) {
       // results will contain the results of the query
       // fields will contain information about the returned results fields (if any)
       console.log(results);
-      res.send('<html><body><h2>Database Data</h2><p>' +  JSON.stringify(results) + "hey" + ' </p></body></html>');
+      var resultss = "";
+      var i = 1;
+      results.forEach(element => {
+        resultss = resultss + ("Customer "+i+" - name: "+element.name + " address: " + element.address + "<br>");
+        i++;
+      });
+      res.send('<html><body><h2>Database Data</h2><p>' +  resultss  + ' </p></body></html>');
     });
     
   connection.end();
